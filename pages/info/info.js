03068a0f999802d2b932ea16f858407708036c3c
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2022-02-04 00:57:32 +0800

    Update info.js

--- pages/info/info.js
+++ pages/info/info.js
@@ -2,36 +2,14 @@
 
 import {request} from '../../lib/request.js';
 import regeneratorRuntime from '../../lib/runtime.js';
-import {} from '../../utils/promise.js';
+import { showToast } from '../../utils/promise.js';
 
 Page({
 
   data: {
-    iconsrc: [
-      "../../icons/shoucang.png", 
-      "../../icons/shoucang _click.png"
-    ],
-    gift_info: {
-      id: 0,
-      title: "礼物名称",
-      status: 0,
-      des: "礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n礼物文案\n",
-      url: "../../images/1.jpg",
-      vodurl: 0,
-      uid: 0,
-      boylike: 20,
-      girllike: 20,
-      cid: 0,
-      progress: 58,
-      tag: "礼物标签",
-      buyurl: "礼物获得途径",
-      taglist: [],
-
-      boyper: 50,
-      girlper: 50,
-      price: "礼物价格",
-      is_collect: false
-    }
+    iconsrc: "../../icons/shoucang.png",
+    iconsrcSelect: "../../icons/shoucang _click.png",
+    gift_info: {}
   },
 
   onLoad: function (options) {
@@ -42,17 +20,29 @@ Page({
   // 获取礼物
   async getGiftInfo(id) {
     try {
-      const res = await request({
-        url: '/gift/gift/getGiftById',
+      const {data} = await request({
+        url: '/gift/gift/getGiftById/' + id,
+        method: 'GET',
         data: {
           id
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
         }
       });
-      console.log(res);
 
-      // this.setData({
-      //   gift_info: {}
-      // });
+      if(data.success) {
+        const {gift} = data.data;
+        gift.is_collect = false;
+        this.setData({
+          gift_info: data.data.gift
+        });
+      } else {
+        await showToast({
+          title: '网络异常',
+          icon: 'error'
+        });
+      }
     } catch (err) {
       console.log(err);
     }
@@ -60,12 +50,22 @@ Page({
 
   // 响应收藏
   handleStarOnClick: function(e) {
+    // 更新至data对象
     const {gift_info} = this.data;
     const {is_collect} = gift_info;
     gift_info.is_collect = !is_collect;
     this.setData({
       gift_info
     });
-    wx.setStorageSync('gift_info', gift_info);
+
+    // 更新至存储: 保存礼物id
+    let collect = wx.getStorageSync('collect') || [];
+    const index = collect.indexOf(gift_info.id);
+    if(gift_info.is_collect && index === -1) {
+      collect.push(gift_info.id);
+    } else if (!gift_info.is_collect && index !== -1) {
+      collect.splice(index, 1);
+    }
+    wx.setStorageSync('collect', collect);
   }
 })
