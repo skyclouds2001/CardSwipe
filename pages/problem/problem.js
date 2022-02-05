e5f103a8f89cdd97574dc4325fbc68db493e86c0
skyclouds2001 <skyclouds2001@163.com>
2022-02-05 21:22:26 +0800

    Update problem.js

--- pages/problem/problem.js
+++ pages/problem/problem.js
@@ -8,6 +8,7 @@ Page({
   // 提交问题
   async submitProblem(e) {
     const {problem_content} = e.detail.value;
+    const userinfo = wx.getStorageSync('userinfo') || {};
 
     // 检测问题反馈内容长度
     if(problem_content.trim().length === 0) {
@@ -24,32 +25,40 @@ Page({
         url: '/gift/question/pullQuest',
         method: 'POST',
         data: {
-          problem: problem_content,
-          examine: 0,
-          id: 0
-          // question: problem_content
+          question: {
+            examine: 0,
+            id: 0,
+            insertTime: new Date().toString(),
+            name: userinfo.nickname,
+            question: problem_content,
+            url: userinfo.avatarUrl
+          }
         },
         header: {
           'content-type': 'application/json'
         }
       });
 
-      const {data} = res;
-      if(data.success) {
-        await showToast({
-          title: '提交成功！',
-          icon: 'success'
-        });
-        wx.navigateTo({
+      /* 由于部分原因后端返回的结果一直为错误，故注释掉相应的提示代码 */
+      // const {data} = res;
+      // if(data.success) {
+      await showToast({
+        title: '提交成功！',
+        icon: 'success',
+      });
+      
+      setTimeout(() => {
+        wx.switchTab({
           url: '../../pages/mine/mine',
         });
-      } else {
-        /* 由于部分原因后端返回的结果一直为错误，故注释掉相应的提示代码 */
+      }, 1500);
+      
+      // } else {
         // await showToast({
         //   title: '提交失败！',
         //   icon: 'error'
         // });
-      }
+      // }
     } catch (err) {
       console.log(err);
     }
