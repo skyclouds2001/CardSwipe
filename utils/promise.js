5487d5dc8fc116e4114ea1c15cfa7ff17d79c7fd
skyclouds2001 <skyclouds2001@163.com>
2022-02-08 00:38:08 +0800

    Update promise.js

--- utils/promise.js
+++ utils/promise.js
@@ -1,86 +1,99 @@
 export const getUserProfile = ({
-  desc
+  desc,
+  lang,
 }) => {
   return new Promise((resolve, reject) => {
     wx.getUserProfile({
-      desc,
+      desc: desc ?? ' ',
+      lang: lang ?? 'en',
       success: (res) => {
         resolve(res);
       },
       fail: (err) => {
         reject(err);
-      }
+      },
     });
   });
 };
 
 export const showToast = ({
   title,
-  icon
+  icon,
+  duration,
+  mask,
 }) => {
   return new Promise((resolve, reject) => {
     wx.showToast({
       title,
-      icon,
-      mask: true,
+      icon: icon ?? 'success',
+      mask: mask ?? true,
+      duration: duration ?? 1500,
       success: (res) => {
         resolve(res);
       },
       fail: (err) => {
         reject(err);
-      }
-    })
+      },
+    });
   });
 };
 
-export const login = () => {
+export const login = ({
+  timeout,
+}) => {
   return new Promise((resolve, reject) => {
     wx.login({
-      timeout: 10000,
+      timeout: timeout ?? 10000,
       success: (res) => {
         resolve(res);
       },
       fail: (err) => {
         reject(err);
-      }
-    })
-  })
-}
+      },
+    });
+  });
+};
 
 export const chooseMedia = ({
   count,
   mediaType,
-  sourceType
+  sourceType,
+  maxDuration,
+  sizeType,
+  camera,
 }) => {
   return new Promise((resolve, reject) => {
     wx.chooseMedia({
-      count,
-      mediaType,
-      sourceType,
+      count: count ?? 9,
+      mediaType: mediaType ?? ['image', 'video'],
+      sourceType: sourceType ?? ['album', 'camera'],
+      maxDuration: maxDuration ?? 10,
+      sizeType: sizeType ?? ['original', 'compressed'],
+      camera: camera ?? 'back',
       success: (res) => {
         resolve(res);
       },
       fail: (err) => {
         reject(err);
-      }
-    })
-  })
-}
+      },
+    });
+  });
+};
 
 export const showModal = ({
   title,
-  content
+  content,
 }) => {
   return new Promise((resolve, reject) => {
     wx.showModal({
-      title,
-      content,
+      title: title ?? ' ',
+      content: content ?? ' ',
       success: (res) => {
         resolve(res);
       },
       fail: (err) => {
         reject(err);
-      }
+      },
     });
   });
-}
+};
