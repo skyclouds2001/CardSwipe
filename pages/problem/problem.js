14fb30e2763a7946f3120682407fe4f8b1f4de54
skyclouds2001 <skyclouds2001@163.com>
2022-02-08 00:37:48 +0800

    Update problem.js

--- pages/problem/problem.js
+++ pages/problem/problem.js
@@ -21,19 +21,11 @@ Page({
 
     // 正式提交问题反馈内容
     try {
-      let qus = {
-        examine: 0,
-        id: 0,
-        insertTime: new Date().toString(),
-        name: userinfo.nickName,
-        question: problem_content,
-        url: userinfo.avatarUrl
-      }; console.log(qus);
-      const res = await request({
+      await request({
         url: '/gift/question/pullQuest',
         method: 'POST',
         data: {
-          question: {
+          questionList : {
             examine: 0,
             id: 0,
             insertTime: new Date().toString(),
@@ -45,10 +37,11 @@ Page({
         header: {
           'Content-Type': 'application/json'
         }
-      });console.log(res);
-      /* 由于部分原因后端返回的结果一直为错误，故注释掉相应的提示代码 */
+      });
+      /* 由于后端返回的结果一直报500错误，但监测发现该报错并不影响正常提交问题功能，故注释掉相应的提示代码 */
       // const {data} = res;
       // if(data.success) {
+
       await showToast({
         title: '提交成功！',
         icon: 'success',
