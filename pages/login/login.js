b48676fe59eeef8954e07a3644219118fc58c363
skyclouds2001 <skyclouds2001@163.com>
2022-02-08 19:29:11 +0800

    Update login.js

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,9 +6,6 @@ import {getUserProfile} from '../../utils/promise.js';
 
 Page({
   async handleGetUserInfo(e) {
-    // 加载：如何保证login与userinfo不同时起效？
-    // this.load();
-
     // 正式获取个人信息
     const res = await getUserProfile({
       desc: '获取个人头像及昵称'
@@ -22,35 +19,27 @@ Page({
       url: '/login/wx',
       method: 'POST',
       data: {
-        code
+        code,
       },
       header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      }
-    });console.log(data);
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+    });
 
     if(data.data.user) {
       const {token} = data.data.user;
       const {openid} = data.data.user;
       wx.setStorageSync('token', token);
       wx.setStorageSync('openid', openid);
-    } else {
-      console.log('error in get openid');
     }
 
     wx.navigateBack({
-      delta: 1
+      delta: 1,
+      fail: () => {
+        wx.navigateTo({
+          url: '../../pages/welcome/welcome',
+        });
+      },
     });
   },
-
-  // load() {
-  //   wx.showLoading({
-  //     title: '加载中...',
-  //     mask: true
-  //   });
-
-  //   setTimeout(() => {
-  //     wx.hideLoading();
-  //   }, 1500);
-  // }
-})
+});
