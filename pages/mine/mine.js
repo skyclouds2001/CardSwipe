e0d969ac8b42140a532ec1221e7463794f90e2d0
skyclouds2001 <skyclouds2001@163.com>
2022-02-08 18:28:14 +0800

    Update mine.js

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -6,44 +6,15 @@ import regeneratorRuntime from '../../lib/runtime.js';
 Page({
 
   data: {
-    // 下部选项数组
-    menu_item: [
-      {
-        iconSrc: "../../icons/shoucang.png",
-        iconActiveSrc: "../../icons/shoucang _click.png",
-        text: "我的收藏",
-        isActive: false,
-        url: "../../pages/collect/collect"
-      },
-      // {
-      //   iconSrc: "../../icons/wode.png",
-      //   iconActiveSrc: "../../icons/wode_click.png",
-      //   text: "我的朋友",
-      //   isActive: false,
-      //   url: "../../pages/friend/friend"
-      // },
-      {
-        iconSrc: "../../icons/xinfeng.png",
-        iconActiveSrc: "../../icons/xinfeng_click.png",
-        text: "问题反馈",
-        isActive: false,
-        url: "../../pages/problem/problem"
-      },
-      {
-        iconSrc: "../../icons/shaogouwu.png",
-        iconActiveSrc: "../../icons/shaogouwu_click.png",
-        text: "礼物添加",
-        isActive: false,
-        url: "../../pages/add/add"
-      }
-    ],
     // 头像链接
     imgurl: '../../images/1.jpg',
     // 昵称
-    nickname: '昵称'
+    nickname: '昵称',
+    // 标记活跃的项
+    isActive: -1,
   },
 
-  onShow: function (options) {
+  onLoad: function () {
     try {
       const userinfo = wx.getStorageSync('userinfo') || [];
       if(userinfo.length !== 0) {
@@ -61,15 +32,17 @@ Page({
   handleOnChange(e) {
     const {id} = e.currentTarget.dataset;
     const {type} = e;
-    let {menu_item} = this.data;
 
-    if(type === "touchstart")
-      menu_item[id].isActive = true;
-    if(type === "touchend")
-      menu_item[id].isActive = false;
-    
-    this.setData({
-      menu_item
-    });
-  }
-})
+    if(type === "touchstart") {
+      this.setData({
+        isActive: id
+      });
+    }
+    if(type === "touchend") {
+      this.setData({
+        isActive: -1
+      });
+    }
+  },
+
+});
