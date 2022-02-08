043686b01f66341ef10c74b044ec426cc60fe022
skyclouds2001 <skyclouds2001@163.com>
2022-02-08 21:15:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,4 +20,30 @@ App({
     
   },
 
+  // 版本号比较代码：来源微信开发文档
+  compareVersion(v1, v2) {
+    v1 = v1.split('.');
+    v2 = v2.split('.');
+    const len = Math.max(v1.length, v2.length);
+  
+    while (v1.length < len) {
+      v1.push('0');
+    }
+    while (v2.length < len) {
+      v2.push('0');
+    }
+  
+    for (let i = 0; i < len; i++) {
+      const num1 = parseInt(v1[i]);
+      const num2 = parseInt(v2[i]);
+  
+      if (num1 > num2) {
+        return 1;
+      } else if (num1 < num2) {
+        return -1;
+      }
+    }
+    return 0;
+  },  
+
 });
