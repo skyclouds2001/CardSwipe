d8f5e4e5112166c51fe9f35f2435ba0e12a95186
skyclouds2001 <skyclouds2001@163.com>
2022-02-18 22:26:26 +0800

    更新app以适配导入colorui组件

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ import {login} from './utils/promise.js';
 App({
 
   onLaunch: function () {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;  
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      },
+    });
   },
 
   onUnhandledRejection: function (e) {
